1c3404b7b18f300313dfab671f600a48d1862e5d
Anh Tra <anhttra-mac@MacBook-Pro.local>
2020-08-29 11:58:56 +0700

    init

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,64 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <sidebar-menu :menu="sidebar.menu" :width="'200px'" :collapsed="sidebar.collapsed">
+      <div slot="header">
+        <div class="header__avatar">
+            <img class="header__avatar-img" src="./assets/avatar.png" alt="98redoc">
+        </div>
+      </div>
+      <i class="fa fa-chevron-right" slot="dropdown-icon"></i>
+      <i class="fa fa-arrows-h" slot="toggle-icon"></i>
+    </sidebar-menu>
+
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
+    </div> -->
+
     <router-view/>
   </div>
 </template>
+
+<script>
+import { SidebarMenu } from 'vue-sidebar-menu'
+export default {
+  components: {
+    SidebarMenu
+  },
+  created() {
+    window.addEventListener('resize', () => {
+        if ( window.innerWidth <= 960 ) {
+            this.sidebar.collapsed = true
+        } else if ( window.innerWidth > 960) {
+            this.sidebar.collapsed = false
+        }
+    });
+  },
+  data() {
+    return {
+      sidebar: {
+        collapsed: window.innerWidth <= 960 ? true : false,
+        menu: [
+          {
+              href: {
+                name: 'Home'
+              },
+              title: 'Home',
+              icon: 'fa fa-home'
+          },
+          {
+              href: {
+                name: 'About'
+              },
+              title: 'About',
+              icon: 'fa fa-info  '
+          },
+        ],
+      }
+    }  
+  }
+}
+</script>
 
 <style>
 #app {
@@ -28,5 +80,19 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+.v-sidebar-menu .vsm--header {
+  text-align: center;
+}
+
+.header__avatar {
+    margin: 1rem 0 1rem 0;
+    display: flex;
+    justify-content: center;
+}
+
+.header__avatar-img {
+    width: 80%;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-
+import router from './router' 
 Vue.config.productionTip = false
+
+import VueSidebarMenu from 'vue-sidebar-menu'
+import 'vue-sidebar-menu/dist/vue-sidebar-menu.css'
+Vue.use(VueSidebarMenu)
+
+import 'font-awesome/css/font-awesome.css'
 
 new Vue({
   router,
